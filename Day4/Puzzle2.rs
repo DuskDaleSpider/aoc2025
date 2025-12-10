abea13ee92922e1ef20a060f151ca4e3d5b5ce28
Dakota Clark <dakota.ray.clark@gmail.com>
2025-12-10 17:45:22 -0600

    Day 4 Complete

--- Day4/Puzzle2.rs
+++ Day4/Puzzle2.rs
@@ -1,61 +1,80 @@
 use std::fs;
-use std::convert::TryInto;
 
 fn main() {
     let file_path = "./input.txt";
 
-    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let mut rows: Vec<&str> = input.split("\n").collect();
-    let num_rows:isize = (rows.len() - 1).try_into().unwrap(); //split above is adding an extra row
-    let num_cols:isize = rows[0].len().try_into().unwrap();
-    let paper_char = "@";
+    let input: Vec<u8> = fs::read(file_path).expect("Should have been able to read the file");
+    let mut rows: Vec<Vec<u8>> = {
+        let mut temp: Vec<Vec<u8>> = Vec::new();
+        for row in input.split(|space| *space == b'\n') {
+           if row.len() != 0 { temp.push(Vec::from(row)); } 
+        }
+        temp
+    };
+    let roll_char = '@';
     let mut total = 0;
     let mut num_removed = 1;
+    let mut num_iter = 1;
 
     while num_removed > 0 {
         num_removed = 0;
-        for x in 0isize..num_rows {
-            let mut temp = String::from(rows[x as usize]);
-            for y in 0isize..num_cols {
-                if temp[y as usize..=y as usize] == *paper_char {
+        println!("========== Step {} =========", num_iter);
+        for (x, row) in rows.clone().into_iter().enumerate() {
+            for (y, space) in row.clone().into_iter().enumerate() {
+                if space == roll_char as u8 {
                     let mut available_areas = 0;
                     
-                    for x2 in -1isize..=1 {
-                        let x_index = x - x2;
-                        if x_index < 0 || x_index >= num_rows {
-                            available_areas += 3;
-                            continue;
-                        }
-                        for y2 in -1isize..=1 {
-                            let y_index = y - y2;
-                            if y_index < 0 || y_index >= num_cols {
-                                available_areas += 1;
-                                continue;
-                            }
-                            let space = &temp[y_index as usize..=y_index as usize];
-                            if space == "." {
+                    let start_offset = if x == 0 {
+                        available_areas += 3;
+                        0
+                    } else { 1 };
+                    let end_offset = if x == rows.len() - 1 {
+                        available_areas += 3;
+                        0
+                    } else { 1 };
+
+                    for x2 in (x - start_offset)..=(x + end_offset) {
+                        let start_offset = if y == 0 {
+                            available_areas += 1;
+                            0
+                        } else { 1 };
+                        let end_offset = if y == row.len() - 1 {
+                            available_areas += 1;
+                            0
+                        } else { 1 };
+
+                        for y2 in (y - start_offset)..=(y + end_offset) {
+                            if rows[x2][y2] == b'.' {
                                 available_areas += 1;
                             }
                         }
                     }
 
-                    if available_areas >  4 {
-                        temp.replace_range(y as usize..=y as usize, ".");
+                    if available_areas > 4 {
                         total += 1;
                         num_removed += 1;
+                        rows[x][y] = b'.';
                         print!("X");
                     } else {
-                        print!("{}", &rows[x as usize][y as usize..=y as usize]);
+                        print!("{}", rows[x][y] as char);
                     }
-                } else {
-                    print!("{}", &rows[x as usize][y as usize..=y as usize]);
-                }
-            }
-            rows[x as usize] = &temp;
-        }
 
-        println!("\n============= Num Removed: {} ===============", num_removed);
+                } else {
+                    print!("{}", space as char);
+                }
+
+                //print!("{}", space as char);
+            }
+            println!("");
+        }
+        println!("Removed {} rolls", num_removed);
+        println!("");
+        num_iter += 1;
     }
 
-    println!("Total rolls that are accessable: {}", total);
+    let num_rows = rows.len();
+    let num_cols = rows[0].len();
+
+    println!("Grid size: {} x {}", num_rows, num_cols);
+    println!("Total rolls removed: {}", total);
 }
