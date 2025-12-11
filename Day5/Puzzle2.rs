54372609bffaf846df0a696f5c9c02076847d9ad
Dakota Clark <dakota.ray.clark@gmail.com>
2025-12-11 01:48:16 -0600

    Day 5 complete

--- Day5/Puzzle2.rs
+++ Day5/Puzzle2.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 #[derive(Debug)]
 #[derive(Clone)]
+#[derive(Copy)]
 struct Range {
     start: i64,
     end: i64
@@ -13,78 +14,91 @@ fn main() {
 
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let lines: Vec<&str> = input.trim().split("\n\n").collect();
-    let mut ranges: Vec<Range> = Vec::new();
+    let mut ranges: Vec<Option<Range>> = Vec::new();
     let mut total = 0;
     // Parsing
     for range in lines[0].split("\n") {
         let nums: Vec<&str> = range.split("-").collect();
-        ranges.push(Range {
+        ranges.push(Some(Range {
             start: nums[0].parse::<i64>().unwrap(),
             end: nums[1].parse::<i64>().unwrap(),
-        });
+        }));
     }
 
-    for (i, mut range) in ranges.clone().into_iter().enumerate() {
-        let mut does_swallow = false;
-        let mut largest_swallow = Range {
-            start: 0,
-            end: 0
-        }; 
-
-        if  i == 0 {
-            total += (range.end - range.start) + 1;
-            continue;
-        }
-        // loop through previously calculated id ranges, see if any overlaps with current and
-        // calculate new range start or end value
-        for j in 0..=i-1 {
-            if range.start >= ranges[j].start && range.end <= ranges[j].end {
-                // current range is contained wholely in the prev
-                // set total range to -1 so total caclution evens out
-                // also reset does_swallow if it's true
-                does_swallow = false;
-                range.start = 0;
-                range.end = -1;
-                break;
-            }
-
-            if range.start > ranges[j].end {
-                // doesn't overlap
-                continue;
-            }else{
-                // overlaps, figure out new start/end
-                if range.start > ranges[j].start {
-                    if ranges[j].end + 1 <= range.end {
-                        // set current range start to prev range end + 1, then move to next prev
-                        range.start = ranges[j].end + 1;
-                        continue;
-                    }
-                } else if range.end >= ranges[j].end {
-                    // Current range swallows a prev one 
-                    // check agaisnt prev largest swallow if applicable
-                    if !does_swallow {
-                        does_swallow = true;
-                        largest_swallow = ranges[j].clone();
-                    } else {
-                        if ranges[j].start <= largest_swallow.start && ranges[j].end >= largest_swallow.end {
-                            largest_swallow = ranges[j].clone();
+    //Throw out any ranges that are contained inside another
+    for (i, current_range) in ranges.clone().into_iter().enumerate() {
+        match current_range {
+            Some(range) => {
+                for (j, comp_range) in ranges.clone().into_iter().enumerate() {
+                    match comp_range {
+                        Some(com_range) => {
+                            if com_range.start <= range.start && com_range.end >= range.end && i != j {
+                                ranges[i] = None;
+                            }
+                        },
+                        None => {
+                            //do nothing
                         }
                     }
-                    continue;
-                } else if range.end >= ranges[j].start {
-                    if ranges[j].start - 1 >= range.start {
-                        range.end = ranges[j].start - 1;
-                        continue;
-                    }
                 }
+            },
+            None => {
+                //do nothing
             }
         }
- 
-        //if current did swallow a prev, find the differece
-        if does_swallow {
-            total += (largest_swallow.start - range.start) + (range.end - largest_swallow.end);
-        } else {
-            total += (range.end - range.start) + 1;
+    }
+
+    for (i, range) in ranges.clone().into_iter().enumerate() {
+        match range {
+            Some(mut r) => {
+                if  i == 0 {
+                    total += (r.end - r.start) + 1;
+                    continue;
+                }
+                // loop through previously calculated id ranges, see if any overlaps with current and
+                // calculate new range start or end value
+                for j in 0..=i-1 {
+                    match ranges[j] {
+                        Some(r2) => {
+                            // shouldn't need this but the removal above doesn't seem to get all
+                            // was good enough for the correct answer though
+                            if r.start >= r2.start && r.end <= r2.end {
+                                // current range is contained wholely in the prev
+                                // set total range to -1 so total calculation evens out
+                                r.start = 0;
+                                r.end = -1;
+                                break;
+                            }
+
+                            if r.start > r2.end {
+                                // doesn't overlap
+                                continue;
+                            }else{
+                                // overlaps, figure out new start/end
+                                if r.start > r2.start {
+                                    if r2.end + 1 <= r.end {
+                                        // set current range start to prev range end + 1, then continue on to next prev
+                                        r.start = r2.end + 1;
+                                        continue;
+                                    }
+                                } else if r.end >= r2.start {
+                                    if r2.start - 1 >= r.start {
+                                        r.end = r2.start - 1;
+                                        continue;
+                                    }
+                                }
+                            }
+                        },
+                        None => {
+                            //do nothing
+                        }
+                    }
+                }
+                total += (r.end - r.start) + 1;
+            },
+            None => {
+                //do nothing
+            }
         }
     }
 
